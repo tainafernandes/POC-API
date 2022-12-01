ab43225165402bcbd0c29bbe97551a29bb253293
tainafernandes <tainafefe@gmail.com>
2022-12-01 13:25:28 -0300

    first commit

---
+++ src/main/java/io/github/tainafernandes/POCAPI/PocApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.tainafernandes.POCAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tainafernandes/POCAPI/PocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tainafernandes.POCAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
